83f17cacf50b2710f610abc2fa1da0672ce55768
yue <p12345678529@gmail.com>
2025-10-11 00:56:13 +0800

    change to FSM structure

--- RTL/TPU.v
+++ RTL/TPU.v
@@ -95,92 +95,123 @@ systolic_array SA (
 
 reg [7:0] M_reg, N_reg, K_reg;
 
-always @(posedge clk or rst_n) begin
+localparam IDLE = 3'd0;
+localparam LOAD = 3'd1;
+localparam CALC = 3'd2;
+localparam WRITEBACK = 3'd3;
+
+reg [2:0] state, next_state;
+reg [7:0] load_cnt;
+reg [7:0] calc_cnt;
+reg [7:0] wb_cnt;
+
+always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
-        counter <= 0;
-        busy <= 0;
-        C_wr_en <= 1'b0;
-
-        A_data_in <= 127'b0;
-        A_wr_en <= 1'b0;
-        A_index <= 1'b0;
-
-
-        B_data_in <= 127'b0;
-        B_wr_en <= 1'b0;
-        B_index <= 1'b0;
-
-        start <= 1'b0;
-
-        A_buffer_in <= 32'b0;
-        B_buffer_in <= 32'b0;
-
-        data_out_counter <= 1'b0;
-        start_data_out <= 1'b0;
-        M_reg <= 8'b0;
-        N_reg <= 8'b0;
-        K_reg <= 8'b0;
-
+        state <= IDLE;
     end
     else begin
-        if(in_valid) begin
-            M_reg <= M;
-            N_reg <= N;
-            K_reg <= K;
-            start <= 1'b1;
-            busy <= 1'b1;
-            A_index <= 0;
-            B_index <= 0;
-        end
-
-        if(start) begin
-            if(counter < K_reg) begin
-                A_index <= A_index + 1;
-                B_index <= B_index + 1;
-                A_buffer_in <= A_data_out;
-                B_buffer_in <= B_data_out;
-            end
-            else begin
-                A_buffer_in <= 32'b0;
-                B_buffer_in <= 32'b0;
-            end
-            counter <= counter + 1;
-        end
-        else begin
-            A_buffer_in <= 32'b0;
-            B_buffer_in <= 32'b0;
-        end
-
-
-        if(counter >= K_reg + 6) begin
-            start <= 1'b0;
-            start_data_out <= 1'b1;
-            C_wr_en <= 1'b1;
-        end
-        else begin
-            C_wr_en <= 1'b0;
-        end
-
-        if(start_data_out) begin
-            if(data_out_counter == 4) begin
-                busy <= 0;
-                data_out_counter <= 0;
-                start_data_out <= 0;
-                counter <= 0;
-            end
-            else begin
-                data_out_counter <= data_out_counter + 1;
-            end
-        end
-        else begin
-            data_out_counter <= 0;
-        end
+        state <= next_state;
     end
 end
 
 always @(*) begin
-    C_index = data_out_counter;
-    C_data_in = c_out[data_out_counter];
+    case(state)
+        IDLE: begin
+            if(in_valid)
+                next_state = LOAD;
+            else
+                next_state = IDLE;
+        end
+        LOAD: begin
+            if(load_cnt >= K_reg - 1)
+                next_state = CALC;
+            else
+                next_state = LOAD;
+        end
+        CALC: begin
+            if(calc_cnt >= 6)
+                next_state = WRITEBACK;
+            else
+                next_state = CALC;
+        end
+        WRITEBACK: begin
+            if(wb_cnt >= 3)
+                next_state = IDLE;
+            else
+                next_state = WRITEBACK;
+        end
+        default: next_state = IDLE;
+    endcase
+end
+
+// Counter and control logic
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+        busy <= 0;
+        A_wr_en <= 0;
+        A_index <= 0;
+        A_data_in <= 0;
+        B_wr_en <= 0;
+        B_index <= 0;
+        B_data_in <= 0;
+        C_wr_en <= 0;
+        C_index <= 0;
+        A_buffer_in <= 0;
+        B_buffer_in <= 0;
+        M_reg <= 0;
+        N_reg <= 0;
+        K_reg <= 0;
+        load_cnt <= 0;
+        calc_cnt <= 0;
+        wb_cnt <= 0;
+    end
+    else begin
+        case(state)
+            IDLE: begin
+
+                if(in_valid) begin
+                    M_reg <= M;
+                    N_reg <= N;
+                    K_reg <= K;
+                    busy <= 1;
+                    A_index <= 0;
+                    B_index <= 0;
+                end
+                else begin
+                    busy <= 0;
+                    C_wr_en <= 0;
+                    load_cnt <= 0;
+                    calc_cnt <= 0;
+                    wb_cnt <= 0;
+                    A_buffer_in <= 0;
+                    B_buffer_in <= 0;
+                end
+            end
+            LOAD: begin
+                A_index <= A_index + 1;
+                B_index <= B_index + 1;
+                A_buffer_in <= A_data_out;
+                B_buffer_in <= B_data_out;
+                load_cnt <= load_cnt + 1;
+            end
+            CALC: begin
+                A_buffer_in <= 0;
+                B_buffer_in <= 0;
+                calc_cnt <= calc_cnt + 1;
+            end
+            WRITEBACK: begin
+                C_wr_en <= 1;
+                C_index <= wb_cnt;
+                C_data_in <= c_out[wb_cnt];
+                wb_cnt <= wb_cnt + 1;
+                if(wb_cnt >= 3) begin
+                    load_cnt <= 0;
+                    calc_cnt <= 0;
+                    wb_cnt <= 0;
+                end
+            end
+        endcase
+    end
 end
 
 
